8948852a2ae2080e09c82a102f7e71b758a6a1d5
Leo Aqua Felix <contact@leoaqua.de>
2024-11-17 15:01:34 +0100

    Always remember: plan your project beforehand if it is bigger than 300 lines of code and DONT start coding and "see how it goes"

--- AoPlugin.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QMainWindow
-
-
-class AoPlugin(QMainWindow):
-    plugin_name: str = None
-    plugin_icon: str = None
-
-    def __init__(self):
-        super().__init__()

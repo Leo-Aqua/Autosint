05214edd4e6120318f359a03ae3c59ca6ec1ced5
Leo Aqua Felix <contact@leoaqua.de>
2024-11-17 12:19:23 +0100

    Added: settings, icon Changed: Window title, Plugin class detection

--- autosint.py
+++ autosint.py
@@ -12,13 +12,21 @@ from PySide6.QtWidgets import (
     QVBoxLayout,
     QRadioButton,
     QScrollArea,
+    QMessageBox,
 )
 import sys
 import os
+import inspect
+from AoPlugin import AoPlugin
+import json
+
+# TODO : Add execution functionality
+# TODO : Add recursion to the plugin system
 
 
-class OptionsTab(QMainWindow):  # TODO: Load settings from file (if possible)
+class OptionsTab(QMainWindow):
     save_signal = Signal(dict)  # Define a custom signal
+    reset_signal = Signal()
 
     def __init__(self):
         super().__init__()
@@ -39,12 +47,54 @@ class OptionsTab(QMainWindow):  # TODO: Load settings from file (if possible)
         save_button.setDefault(True)
         layout.addWidget(save_button)
 
+        reset_button = QPushButton("Reset to defaults")
+        reset_button.setFixedSize(100, 30)
+        layout.addStretch()
+        layout.addWidget(reset_button)
+
+        self.initiate_settings()
+
         save_button.clicked.connect(self.emit_settings)
+        reset_button.clicked.connect(self.emit_reset)
+
+    def emit_reset(self):
+        self.reset_signal.emit()
+        close_message = QMessageBox()
+        close_message.setText(
+            "An application restart is required to reset the settings. Do you want to clode the program now ?"
+        )
+        close_message.setWindowTitle("Reset settings")
+        close_message.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        close_message.setDefaultButton(QMessageBox.Yes)
+        close_message.setIcon(QMessageBox.Question)
+        close_message.setWindowIcon(QIcon(os.path.dirname(__file__) + "/icon.png"))
+        if close_message.exec_() == QMessageBox.Yes:
+            sys.exit(0)
 
     def emit_settings(self):
         settings = {"vertical": self.tab_orientation_v.isChecked()}
 
         self.save_signal.emit(settings)
+
+    def load_settings(self):
+        try:
+            settings = json.load(
+                open(os.path.dirname(__file__) + "/config/settings.json", "r")
+            )
+        except:
+            settings = json.load(
+                open(os.path.dirname(__file__) + "/config/settings_default.json", "r")
+            )
+        return settings
+
+    def initiate_settings(self):
+
+        settings = self.load_settings()
+
+        if settings["vertical"]:
+            self.tab_orientation_v.setChecked(True)
+        else:
+            self.tab_orientation_h.setChecked(False)
 
 
 class MainWindow(QMainWindow):
@@ -63,10 +113,12 @@ class MainWindow(QMainWindow):
         """
         super().__init__()
 
-        self.setWindowTitle("autOsint")
+        self.setWindowTitle("Autosint")
         self.resize(800, 500)
+        self.setWindowIcon(QIcon(os.path.dirname(__file__) + "/icon.png"))
 
         self.plugins = []
+        self.settings = {}
 
         main_grid_layout = QGridLayout()
         main_grid_layout.setContentsMargins(5, 5, 5, 5)
@@ -106,10 +158,14 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(widget)
 
+        ## Load settings
+        self.load_settings()
+        self.apply_settings()
+
         ## Events
         load_button.clicked.connect(self.load_plugins)
-        app_tab = OptionsTab()
-        app_tab.save_signal.connect(self.apply_options)
+        self.options_tab_instance.save_signal.connect(self.receive_settings)
+        self.options_tab_instance.reset_signal.connect(self.reset_settings)
 
     def load_plugins(self):
         old_plugins = set(self.plugins)
@@ -137,10 +193,19 @@ class MainWindow(QMainWindow):
         # Add or update plugins in the tab widget
         for plugin_name in self.plugins:
             full_plugin_name = f"plugins.{plugin_name}"
+            plugin_module = __import__(full_plugin_name, fromlist=["*"])
 
-            plugin_module = __import__(full_plugin_name, fromlist=["Plugin"])
-            if hasattr(plugin_module, "Plugin"):
-                ao_plugin_instance = plugin_module.Plugin()
+            # Find the class inheriting from AoPlugin dynamically
+            plugin_class = None
+            for name, obj in inspect.getmembers(plugin_module, inspect.isclass):
+                if (
+                    issubclass(obj, AoPlugin) and obj is not AoPlugin
+                ):  # Avoid the base class itself
+                    plugin_class = obj
+                    break
+
+            if plugin_class:
+                ao_plugin_instance = plugin_class()
                 plugin_tab = self.tab_widget.addTab(
                     ao_plugin_instance, ao_plugin_instance.plugin_name
                 )
@@ -160,15 +225,49 @@ class MainWindow(QMainWindow):
         self.options_tab_instance = OptionsTab()
         self.option_tab = self.tab_widget.addTab(self.options_tab_instance, "Options")
         # Connect the signal
-        self.options_tab_instance.save_signal.connect(self.apply_options)
+        self.options_tab_instance.save_signal.connect(self.receive_settings)
         self.tab_widget.tabBar().setTabTextColor(self.option_tab, QColor("lightblue"))
-        self.tab_widget.setTabIcon(self.option_tab, QIcon("fugue/icons/gear.png"))
+        self.tab_widget.setTabIcon(
+            self.option_tab, QIcon(os.path.dirname(__file__) + "/fugue/icons/gear.png")
+        )
 
-    def apply_options(self, e):  # TODO: Save settings in a file
-        if e["vertical"]:
+    def apply_settings(self): 
+
+        if self.settings["vertical"]:
             self.tab_widget.setTabPosition(QTabWidget.East)
         else:
             self.tab_widget.setTabPosition(QTabWidget.North)
+
+        self.save_settings()
+
+    def save_settings(self):
+        json.dump(
+            self.settings,
+            open(os.path.dirname(__file__) + "/config/settings.json", "w"),
+        )
+
+    def load_settings(self):
+        try:
+            self.settings = json.load(
+                open(os.path.dirname(__file__) + "/config/settings.json", "r")
+            )
+        except:
+            self.settings = json.load(
+                open(os.path.dirname(__file__) + "/config/settings_default.json", "r")
+            )
+        return self.settings
+
+    def reset_settings(self, e=None):
+        self.settings = json.load(
+            open(os.path.dirname(__file__) + "/config/settings_default.json", "r")
+        )
+        self.save_settings()
+
+    def receive_settings(self, e):
+        self.settings = e
+        self.save_settings()
+        self.load_settings()
+        self.apply_settings()
 
 
 app = QApplication(sys.argv)

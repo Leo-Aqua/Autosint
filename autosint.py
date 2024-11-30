7680a351252109df7483394e541c1f147f4c66c5
Leo Aqua Felix <contact@leoaqua.de>
2024-11-30 22:36:14 +0100

    added listwidget updating

--- autosint.py
+++ autosint.py
@@ -8,32 +8,31 @@ import inspect
 import importlib
 import autosint
 import logging
+import coloredlogs
+import re
 
 
 class Autosint(QtWidgets.QMainWindow):
     def __init__(self):
         super(Autosint, self).__init__()
-        logging.basicConfig(
-            level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
-        )
+        # logging.basicConfig(
+        #     level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+        # )
+        coloredlogs.install(level="DEBUG")
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-
+        logging.info("Initialized ui")
+        self.load_plugins()
         # Events
         self.ui.load_plugin_action.triggered.connect(self.load_plugins)
         self.ui.load_plugins_button.clicked.connect(self.load_plugins)
+        self.ui.plugin_add_button.clicked.connect(self.add_plugin)
+        # self.ui.plugin_remove_button.clicked.connect(self.remove_plugin) # TODO Implement
         self.show()
 
     def load_plugins(self):
-        def check_valid_plugin(plugin):
-            # Check if setupUi method exists
 
-            if "setupUi" in plugin.__dict__:
-                logging.debug(f"setupUi() exists in {str(plugin)}")
-            else:
-                logging.warning(f"{str(plugin)} does not have a setupUi function!")
-
-        self.plugins = {}
+        self.plugins = []
         # Get all Python files in the plugins directory
         plugin_files = [f for f in os.listdir("plugins") if f.endswith(".py")]
 
@@ -47,30 +46,74 @@ class Autosint(QtWidgets.QMainWindow):
                 logging.error(f"Error importing {plugin_name}: {e}")
                 continue
 
+            logging.debug(inspect.getmembers(module, inspect.isclass))
             # Find all classes in the module that inherit from Plugin
             for name, obj in inspect.getmembers(module, inspect.isclass):
-                for cls in obj.__subclasses__():
-                    if issubclass(cls, autosint.Plugin):
-                        check_valid_plugin(cls)
-                        self.plugins[cls.__name__] = cls
+                if obj is autosint.Plugin:  # Skip if class is autosint.Plugin()
+                    continue
 
-            logging.debug("Loaded plugins: " + str(self.plugins))
+                if (
+                    obj.__base__ is autosint.Plugin
+                ):  # Check if parent class is autosint.Plugin()
+                    self.plugins.append(
+                        {
+                            "class": obj,
+                            "active": False,
+                            "filename": plugin_file,
+                            "module": module,
+                        }
+                    )
+
+        logging.debug("Loaded plugins: " + str(self.plugins))
+        self.ui.statusbar.showMessage(f"Loaded {str(len(self.plugins))} plugins.", 3000)
+        self.updatePluginList()
 
     def get_plugins(self):
         return self.plugins
 
+    def updatePluginList(self):
+        self.ui.plugin_list.clear()
+        for plugin in self.plugins:
+
+            self.ui.plugin_list.addItem(
+                f"{plugin["class"]().name} - v{plugin["class"]().version}"
+            )
+            items = [
+                self.ui.plugin_list.item(x) for x in range(self.ui.plugin_list.count())
+            ]
+            self.ui.plugin_list.item(len(items) - 1).setToolTip(
+                f"Name: {plugin["class"]().name}\nVersion: {plugin["class"]().version}\nDescription: {plugin["class"]().description}\nSource: {plugin["module"].__name__}"
+            )
+
+    def add_plugin(self):
+        try:
+            selected_row = self.ui.plugin_list.selectedIndexes()[0].row()
+            plugin = self.plugins[selected_row]
+            logging.debug(plugin)
+        except IndexError:
+            pass  # Do nothig in case nothing is selected
+
 
 class Plugin(QtWidgets.QWidget):
-    plugin_name: str = None
-    plugin_icon: QtGui.QIcon = None
 
-    input_fields: list = None
-    output_fields: list = None
-
-    compatible_plugins: list = None
-
-    def __init__(self):
+    def __init__(
+        self,
+        name: str,
+        input_fields: list,
+        output_fields: list,
+        compatible_plugins: list,
+        version: str,
+        icon: QtGui.QIcon = None,
+        description: str = None,
+    ):
         super(Plugin, self).__init__()
+        self.name = name
+        self.input_fields = input_fields
+        self.output_fields = output_fields
+        self.compatible_plugins = compatible_plugins
+        self.icon = icon
+        self.description = description
+        self.version = version
 
     def setupUi(self):
         pass
@@ -78,7 +121,8 @@ class Plugin(QtWidgets.QWidget):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication()
-    app.setStyle("Fusion")
+    print(QtWidgets.QStyleFactory.keys())
+    app.setStyle(QtWidgets.QStyleFactory.create("Fusion"))
     window = Autosint()
     window.show()
     sys.exit(app.exec())
